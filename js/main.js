9eb5df2c5fcb40ea4fa9ee08c21005ac5790d217
0alif <alifhossain003@gmail.com>
2021-09-02 04:17:11 +0600

    load data

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-
+// load data
 const getValue = () => {
-    const searchFiled = document.getElementById('search-filed').value;
-    console.log(searchFiled);
+    const searchText = document.getElementById('search-text').value;
+    console.log(searchText);
+    fetch(`http://openlibrary.org/search.json?q=${searchText}`)
+        .then(res => res.json())
+        .then(data => bookDetails(data.docs));
+}
+// total books number found by search
+const bookDetails = book => {
+    const totalResult = document.getElementById('total-result');
+    totalResult.innerText = `${book.length} result found`;
+    book.forEach(element => {
+        displayDetails(element);
+    });
+}
+// display book details
+const displayDetails = details => {
+    const books = document.getElementById('books');
+    console.log(details);
 }
