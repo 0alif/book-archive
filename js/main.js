ccb9e4695c2e2540fe5ddfefa4bc69182056f547
0alif <alifhossain003@gmail.com>
2021-09-02 07:57:45 +0600

    show books

--- js/main.js
+++ js/main.js
@@ -1,21 +1,44 @@
+const errorMessage = document.getElementById('error-msg');
 // load data
 const getValue = () => {
     const searchText = document.getElementById('search-text').value;
-    console.log(searchText);
-    fetch(`http://openlibrary.org/search.json?q=${searchText}`)
-        .then(res => res.json())
-        .then(data => bookDetails(data.docs));
+    // Handle error
+    if (searchText === '') {
+        errorMessage.innerText = 'Search field can not be empty';
+    }
+    else {
+        fetch(`http://openlibrary.org/search.json?q=${searchText}`)
+            .then(res => res.json())
+            .then(data => bookDetails(data));
+    }
 }
 // total books number found by search
 const bookDetails = book => {
-    const totalResult = document.getElementById('total-result');
-    totalResult.innerText = `${book.length} result found`;
-    book.forEach(element => {
-        displayDetails(element);
-    });
+    if (book.docs.length === 0) {
+        errorMessage.innerText = 'Sorry No result found';
+    }
+    else {
+        const totalResult = document.getElementById('total-result');
+        totalResult.innerText = `Showing ${book.docs.length} results of ${book.numFound}`;
+        books = book.docs;
+        books.forEach(element => {
+            displayDetails(element);
+        });
+    }
 }
 // display book details
 const displayDetails = details => {
     const books = document.getElementById('books');
-    console.log(details);
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
+    <div class="card">
+    <img src="https://covers.openlibrary.org/b/id/${details.cover_i}-M.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="">${details.title}</h5>
+      <p>Author: ${details.author_name}</p>
+      <p>First publish date: ${details.publish_date}</p>
+    </div>
+  </div>  `;
+    books.appendChild(div);
 }
